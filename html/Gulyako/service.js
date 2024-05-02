7c0d2fb3bc9f0be96dbcb6b6b9ced1346ca195de
Sergei <sergeigulyako@mail.ru>
2024-05-02 13:14:27 +0300

    added dark mode to store page

---
+++ html/Gulyako/service.js
@@ -0,0 +1,9 @@
+export const storage = {
+       setTheme(theme) {
+              localStorage.setItem("theme", theme);
+       },
+       getTheme() {
+              return localStorage.getItem("theme");
+
+       },
+};
