37d228e476600a26860f789bba8c25169e6a5852
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-05-06 21:44:53 +0300

    Add example of contoller and view

---
+++ PortalAboutEverything/PortalAboutEverything/Controllers/GameController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalAboutEverything.Models.Game;
+
+namespace PortalAboutEverything.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            var second = DateTime.Now.Second;
+            var dayName = DateTime.Now.DayOfWeek.ToString();
+            
+            var days = Enum.GetNames<DayOfWeek>().ToList();
+
+            var viewModel = new GameIndexViewModel
+            {
+                Second = second,
+                DayName = dayName,
+                Days = days
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult RateTheGame(GameRateViewModel rateViewModel)
+        {
+            return View(rateViewModel);
+        }
+    }
+}
---
+++ PortalAboutEverything/PortalAboutEverything/Models/Game/GameIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PortalAboutEverything.Models.Game
+{
+    public class GameIndexViewModel
+    {
+        public int Second { get; set; }
+        public string DayName { get; set; }
+        public List<string> Days { get; set; }
+        public List<int> AvailableRates { get; set; } = Enumerable.Range(1, 10).ToList();
+    }
+}
