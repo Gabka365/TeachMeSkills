8290d59a9672302d37b8994c0ad35be9dd5848ff
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-05-07 18:08:51 +0300

    Added empty controller and view

---
+++ PortalAboutEverything/PortalAboutEverything/Controllers/BoardGameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortalAboutEverything.Controllers
+{
+    public class BoardGameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
