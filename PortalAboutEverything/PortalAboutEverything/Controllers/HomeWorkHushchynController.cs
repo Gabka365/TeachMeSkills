3cd6cbfd06f94cd2dd9ab619614a0c0ac3118320
Yura12461 <164777474+Yura12461@users.noreply.github.com>
2024-05-08 17:12:13 +0300

    Add MVC

---
+++ PortalAboutEverything/PortalAboutEverything/Controllers/HomeWorkHushchynController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalAboutEverything.Models.Hushchyn;
+
+namespace PortalAboutEverything.Controllers
+{
+    public class HomeWorkHushchynController : Controller
+    {
+        public IActionResult Index()
+        {
+            var second = DateTime.Now.Second;
+
+            var random = new Random();
+
+            var days = Enum.GetNames<DayOfWeek>().ToList();
+
+            var viewModel = new HomeWorkHushchynViewModel
+            {
+                Second = second,
+                Number = random.Next(1, 100),
+                Days = days,
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Page2(Page2VIewModel page2ViewModel)
+        {
+            return View(page2ViewModel);
+        }
+    }
+}
