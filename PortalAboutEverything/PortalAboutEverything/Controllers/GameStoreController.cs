1a6eaf10e0d347fedbbcb3da8bebd6a38b2c3c5d
IlyaKotenko <146016199+SazanOne@users.noreply.github.com>
2024-05-09 15:58:25 +0300

    Add Folder

---
+++ PortalAboutEverything/PortalAboutEverything/Controllers/GameStoreController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalAboutEverything.Models.GameStore;
+
+namespace PortalAboutEverything.Controllers
+{
+    public class GameStoreController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var viewModel = new GameStoreIndexViewModel
+            {
+
+            };
+
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult RateTheGameStore(RateTheGameStoreViewModel rateTheGameStoreViewModel)
+        {
+
+            return View(rateTheGameStoreViewModel);
+        }
+    }
+
+}
---
+++ PortalAboutEverything/PortalAboutEverything/Models/GameStore/GameStoreIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PortalAboutEverything.Models.GameStore
+{
+    public class GameStoreIndexViewModel
+    {
+        public List<int> AvailableRates { get; set; } = Enumerable.Range(1, 5).ToList();
+
+    }
+}
