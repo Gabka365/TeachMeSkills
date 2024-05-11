ca0f7d9aa9d6f3ae929d2fcd8b8412ccd87d6cd7
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-05-11 16:49:37 +0300

    Added update and delete

--- PortalAboutEverything/PortalAboutEverything/Controllers/BoardGameController.cs
+++ PortalAboutEverything/PortalAboutEverything/Controllers/BoardGameController.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalAboutEverything.Models.BoardGame;
 using PortalAboutEverything.Data.Model;
 using PortalAboutEverything.Data.Repositories;
-using PortalAboutEverything.Models.BoardGame;
-using PortalAboutEverything.Models.Game;
-using System;
 
 namespace PortalAboutEverything.Controllers
 {
@@ -22,6 +19,23 @@ namespace PortalAboutEverything.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BoardGameCreateReviewViewModel boardGameReviewViewModel)
+        {
+            boardGameReviewViewModel.Date = DateTime.Now;
+
+            BoardGameReview review = BuildBoardGameRewievDataModelFromCreate(boardGameReviewViewModel);
+
+            _reviewRepositories.Create(review);
+            return RedirectToAction("FirstBoardGame");
+        }
+
         public IActionResult FirstBoardGame()
         {
             List<BoardGameReviewViewModel> reviewViewModels = _reviewRepositories
@@ -33,24 +47,27 @@ namespace PortalAboutEverything.Controllers
         }
 
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult Update(int id)
         {
-            return View();
+            BoardGameReview reviewForUpdate = _reviewRepositories.Get(id);
+            BoardGameUpdateReviewViewModel viewModel = BuildBoardGameUpdateRewievDataModel(reviewForUpdate);
+
+            return View(viewModel);
         }
 
         [HttpPost]
-        public IActionResult Create(BoardGameReviewViewModel boardGameReviewViewModel)
+        public IActionResult Update(BoardGameUpdateReviewViewModel boardGameReviewViewModel)
         {
-            boardGameReviewViewModel.Date = DateTime.Now;
+            BoardGameReview updatedReview = BuildBoardGameRewievDataModelFromUpdate(boardGameReviewViewModel);
+            _reviewRepositories.Update(updatedReview);
 
-            BoardGameReview review = new()
-            {
-                Name = boardGameReviewViewModel.Name,
-                Date = boardGameReviewViewModel.Date,
-                Text = boardGameReviewViewModel.Text
-            };
+            return RedirectToAction("FirstBoardGame");
+        }
+       
+        public IActionResult Delete(int id)
+        {
+            _reviewRepositories.Delete(id);
 
-            _reviewRepositories.Create(review);
             return RedirectToAction("FirstBoardGame");
         }
 
@@ -58,6 +75,31 @@ namespace PortalAboutEverything.Controllers
             => new BoardGameReviewViewModel
             {
                 Id = review.Id,
+                Name = review.Name,
+                DateInStringFormat = review.Date.ToString("dd.MM.yyyy HH:mm"),
+                Text = review.Text,
+            };
+
+        private BoardGameReview BuildBoardGameRewievDataModelFromCreate(BoardGameCreateReviewViewModel reviewViewModel)
+            => new BoardGameReview
+            {
+                Name = reviewViewModel.Name,
+                Date = reviewViewModel.Date,
+                Text = reviewViewModel.Text,
+            };
+
+        private BoardGameReview BuildBoardGameRewievDataModelFromUpdate(BoardGameUpdateReviewViewModel reviewViewModel)
+            => new BoardGameReview
+            {
+                Id = reviewViewModel.Id,
+                Name = reviewViewModel.Name,
+                Date = reviewViewModel.Date,
+                Text = reviewViewModel.Text,
+            };
+
+        private BoardGameUpdateReviewViewModel BuildBoardGameUpdateRewievDataModel(BoardGameReview review)
+            => new BoardGameUpdateReviewViewModel
+            {
                 Name = review.Name,
                 Date = review.Date,
                 Text = review.Text,
