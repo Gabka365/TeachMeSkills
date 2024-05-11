3231808b2af280878b36842b426d07db6d8060bf
Maksim <maxpotapnev2003@gmail.com>
2024-05-11 19:57:44 +0300

    Add site to portal about everything

---
+++ PortalAboutEverything/PortalAboutEverything/Controllers/BlogController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalAboutEverything.Data.Model;
+using PortalAboutEverything.Data.Repositories;
+using PortalAboutEverything.Models.Blog;
+
+
+namespace PortalAboutEverything.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendingMessage()
+        {
+            var now = DateTime.Now;
+            var name = "Morgan Freeman";
+
+            var viewModel = new BlogIndexViewModel
+            {
+                Now = now,
+                Name = name,
+            };
+
+            return View(viewModel);
+        }
+
+
+        public IActionResult ReceivingMessage(ReceivingDataViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
