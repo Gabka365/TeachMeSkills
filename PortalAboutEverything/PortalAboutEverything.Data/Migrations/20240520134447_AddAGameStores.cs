822b5ce3261f8483771acd0f5e80b072317fdc9f
IlyaKotenko <146016199+SazanOne@users.noreply.github.com>
2024-05-20 20:16:36 +0300

    Update update

--- PortalAboutEverything/PortalAboutEverything.Data/Migrations/20240520134447_AddAGameStores.cs
+++ PortalAboutEverything/PortalAboutEverything.Data/Migrations/20240520134447_AddAGameStores.cs
@@ -10,13 +10,27 @@ namespace PortalAboutEverything.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "GameStores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearOfRelease = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameStores", x => x.Id);
+                });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "GameStores");
         }
     }
 }
