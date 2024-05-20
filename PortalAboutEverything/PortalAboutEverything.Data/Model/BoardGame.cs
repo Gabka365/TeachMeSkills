6c22e0518368efa70ab583b73e25f7d79908cef5
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-05-21 02:00:10 +0300

    Updated tags in BoardGameModel

--- PortalAboutEverything/PortalAboutEverything.Data/Model/BoardGame.cs
+++ PortalAboutEverything/PortalAboutEverything.Data/Model/BoardGame.cs
@@ -7,7 +7,7 @@
         public string MiniTitle { get; set; }
         public string Description { get; set; }
         public string Essence { get; set; }
-        public List<string> Tags { get; set; }
+        public string Tags { get; set; }
         public double Price { get; set; }
         public long ProductCode { get; set; }
         public List<BoardGameReview> Reviews { get; set; }
--- PortalAboutEverything/PortalAboutEverything/Models/BoardGame/BoardGameViewModel.cs
+++ PortalAboutEverything/PortalAboutEverything/Models/BoardGame/BoardGameViewModel.cs
@@ -6,7 +6,7 @@
         public string MiniTitle { get; set; }
         public string Description { get; set; }
         public string Essence { get; set; }
-        public List<string> Tags { get; set; }
+        public string Tags { get; set; }
         public double Price { get; set; }
         public long ProductCode { get; set; }
         public List<BoardGameReviewViewModel> Reviews { get; set; }
