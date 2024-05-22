b5820b614044603ae822564fb6b521c63cbde595
Kpoxu4 <159006608+Kpoxu4@users.noreply.github.com>
2024-05-22 12:07:48 +0300

    add Repositories<T>

---
+++ PortalAboutEverything/PortalAboutEverything.Data/Repositories/RepositoriesTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalAboutEverything.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalAboutEverything.Data.Repositories
+{
+    public abstract class RepositoriesTest<T> where T : class
+    {
+        public PortalDbContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        public RepositoriesTest(PortalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public void Create(T repositories)
+        {
+            _dbSet.Add(repositories);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var repositories = _dbSet.Find(id);
+            _dbSet.Remove(repositories);
+            _dbContext.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T Get(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public abstract void Update(T entity);
+    }
+}
+
--- PortalAboutEverything/PortalAboutEverything.Data/Repositories/TravelingRepositories.cs
+++ PortalAboutEverything/PortalAboutEverything.Data/Repositories/TravelingRepositories.cs
@@ -1,42 +1,14 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using PortalAboutEverything.Data.Model;
 
 namespace PortalAboutEverything.Data.Repositories
 {
-    public class TravelingRepositories
+    public class TravelingRepositories : RepositoriesTest<Traveling>
     {
-        private PortalDbContext _dbContext;
-     
-        public TravelingRepositories(PortalDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        public TravelingRepositories(PortalDbContext dbContext) : base(dbContext) { }
 
-        public void Create(Traveling traveling)
-        {
-            _dbContext.Add(traveling);
-
-            _dbContext.SaveChanges();
-        }
-
-        public void Delete(int id)
-        {
-            var traveling = _dbContext.Travelings
-               .Single(x => x.Id == id);
-            _dbContext.Remove(traveling);
-
-            _dbContext.SaveChanges();
-        }
-
-        public List<Traveling> GetAll()
-        {
-            return _dbContext.Travelings.ToList();
-        }
-
-        public Traveling Get(int id)
-            => _dbContext.Travelings.Single(x => x.Id == id);
-
-        public void Update(Traveling traveling)
+        public override void Update(Traveling traveling)
         {
             var dbTraveling = Get(traveling.Id);
 
@@ -45,5 +17,6 @@ namespace PortalAboutEverything.Data.Repositories
             _dbContext.SaveChanges();
         }
     }
+
 }
 
