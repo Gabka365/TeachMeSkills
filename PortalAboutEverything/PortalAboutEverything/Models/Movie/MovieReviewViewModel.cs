dd285f19dc4452396220b40a524edfef71a21e5f
TatianaNai <tatyana_tat333@mail.ru>
2024-06-13 20:13:09 +0300

    Add option - delete movie's review

--- PortalAboutEverything/PortalAboutEverything/Models/Movie/MovieReviewViewModel.cs
+++ PortalAboutEverything/PortalAboutEverything/Models/Movie/MovieReviewViewModel.cs
@@ -2,6 +2,7 @@
 {
 	public class MovieReviewViewModel
 	{
+		public int Id { get; set; }
 		public int Rate { get; set; }
 		public DateTime DateOfCreation { get; set; }
 		public string Comment { get; set; }
