803cdaef9396017d020252490fd1153dddd62afd
Sergei <sergeigulyako@mail.ru>
2024-06-17 22:41:32 +0300

    fix spaces

--- PortalAboutEverything/PortalAboutEverything.Data/Model/Store/Good.cs
+++ PortalAboutEverything/PortalAboutEverything.Data/Model/Store/Good.cs
@@ -17,7 +17,5 @@ namespace PortalAboutEverything.Data.Model.Store
         public virtual List<GoodReview>? Reviews { get; set; }
 
         public virtual List<User>? UsersWhoLikedTheGood {  get; set; }
-
-
     }
 }
