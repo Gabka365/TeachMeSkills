3421009a58fd834a16a4d20a60ec553a8090351b
ilyakotenko <ilyakotenko98@mail.ru>
2024-06-17 16:58:03 +0300

    Add MaxImage Validation Attribute

---
+++ PortalAboutEverything/PortalAboutEverything/Models/ValidationAttributes/MaxImageGameStoreSizeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortalAboutEverything.Models.ValidationAttributes
+{
+    public class MaxImageGameStoreSizeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value is not IFormFile)
+            {
+                throw new ArgumentException($"you ca use {nameof(MaxImageGameStoreSizeAttribute)} only with IFormFile ");
+
+            }
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
