7350eba85e04d9f0e60fd55cadfa41d9d723d384
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-06-17 21:19:18 +0300

    Example of ajax and debounce

--- PortalAboutEverything/PortalAboutEverything/wwwroot/js/site.js
+++ PortalAboutEverything/PortalAboutEverything/wwwroot/js/site.js
@@ -1,1 +1,54 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+
+
+
+	const boardGames = document.querySelectorAll(".board-game-item");
+
+	hideAllAdminBlocks(boardGames);
+
+
+
+
+	boardGames.forEach((game) => {
+
+		game.addEventListener("click", () => {
+
+			const isCurrentGame = game.classList.contains("active");
+
+
+
+
+			boardGames.forEach((game) => { game.classList.remove("active") });
+
+			hideAllAdminBlocks(boardGames);
+
+			if (isCurrentGame) {
+				return;
+			}
+
+			game.classList.add("active");
+			const adminBlock = game.querySelector(".update-and-delete");
+			if (!adminBlock) {
+				return;
+			}
+
+			adminBlock.style.display = 'flex';
+		});
+
+	});
+	
+
+	function hideAllAdminBlocks(boardGames) {
+		boardGames.forEach((game) => {
+			const adminBlock = game.querySelector(".update-and-delete");
+			if (adminBlock) {
+				adminBlock.style.display = 'none';
+			}
+		});
+	}
+
+});
+
+
+
