8e2a2d0a0f8f262e6812f9e01a8e5ea4c5526f92
IlyaKotenko <146016199+SazanOne@users.noreply.github.com>
2024-06-17 17:40:36 +0300

    update

--- PortalAboutEverything/PortalAboutEverything/Models/ValidationAttributes/MaxImageGameStoreSizeAttribute.cs
+++ PortalAboutEverything/PortalAboutEverything/Models/ValidationAttributes/MaxImageGameStoreSizeAttribute.cs
@@ -1,15 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 
 namespace PortalAboutEverything.Models.ValidationAttributes
 {
     public class MaxImageGameStoreSizeAttribute : ValidationAttribute
     {
+        private int _maxImageWidth;
+        private int _maxImageHeight;
+
+        public MaxImageGameStoreSizeAttribute(int maxImageWidth, int maxImageHeight)
+        {
+            _maxImageWidth = maxImageWidth;
+            _maxImageHeight = maxImageHeight;
+        }
+
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            if(value is not IFormFile)
+            if (value is not IFormFile)
             {
                 throw new ArgumentException($"you ca use {nameof(MaxImageGameStoreSizeAttribute)} only with IFormFile ");
 
+            }
+            var formFile = (IFormFile)value;
+
+            using var stream = formFile.OpenReadStream();
+            var image = Image.FromStream(stream);
+
+            if (image.Width > _maxImageWidth || image.Height > _maxImageHeight)
+            {
+                return new ValidationResult(
+                    $"The maximum width {_maxImageWidth}. " +
+                    $"Maximum height {_maxImageHeight}");
             }
             return base.IsValid(value, validationContext);
         }
