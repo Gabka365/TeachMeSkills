ba9ef71ae643d1212a1393120f6b2b539b1f3dd6
Sergei <sergeigulyako@mail.ru>
2024-06-19 16:53:43 +0300

    fix spaces

--- PortalAboutEverything/PortalAboutEverything/wwwroot/js/Store/service.js
+++ PortalAboutEverything/PortalAboutEverything/wwwroot/js/Store/service.js
@@ -1,8 +1,8 @@
 export const storage = {
-       setTheme(theme) {
-              localStorage.setItem("theme", theme);
-       },
-       getTheme(){
-              return localStorage.getItem("theme");
-       },
+    setTheme(theme) {
+        localStorage.setItem("theme", theme);
+    },
+    getTheme() {
+        return localStorage.getItem("theme");
+    },
 };
