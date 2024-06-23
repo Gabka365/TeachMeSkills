0bea7313596896127584402febb4323ad4cf31c1
TatianaNai <tatyana_tat333@mail.ru>
2024-06-24 00:17:04 +0300

    add movie hub

--- PortalAboutEverything/PortalAboutEverything/Program.cs
+++ PortalAboutEverything/PortalAboutEverything/Program.cs
@@ -76,6 +76,7 @@ app.UseAuthorization(); // May I?
 app.UseMiddleware<LocalizationMiddleware>();
 
 app.MapHub<ChatHub>("/hubs/chat");
+app.MapHub<MovieHub>("/hubs/movie");
 
 app.MapControllerRoute(
     name: "default",
