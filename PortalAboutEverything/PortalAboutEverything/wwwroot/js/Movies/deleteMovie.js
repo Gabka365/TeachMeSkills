181d1f1d354e9716477fc4d94abd67520aee13be
TatianaNai <tatyana_tat333@mail.ru>
2024-06-23 16:39:51 +0300

    correct js delete movie

--- PortalAboutEverything/PortalAboutEverything/wwwroot/js/Movies/deleteMovie.js
+++ PortalAboutEverything/PortalAboutEverything/wwwroot/js/Movies/deleteMovie.js
@@ -1,26 +1,22 @@
 $(document).ready(function () {
 
-	const debounceDeleteMovie = debounce(deleteMovie, 500);
+    $('.deleteMovie').click(function () {
+        const movieToDelete = $('.movie.active');
+        const movieInput = movieToDelete.find('.movieIdDel')
+        const movieId = movieInput.val();
 
-	$('.deleteMovie').click(function () {
-		debounceDeleteMovie.call();
-	});
+        const tableStatisticCellByMovieId = $(`.movieTableCellId[value="${movieId}"]`);
+        const rowToDelete = tableStatisticCellByMovieId.closest('.movieContent');
 
-	function deleteMovie() {
-		const movieToDelete = $('.movie.active');
-		const movieInput = movieToDelete.find('.movieIdDel')
-		const movieId = movieInput.val();
-
-		const tableStatisticCellByMovieId = $(`.movieTableCellId[value="${movieId}"]`);
-		const rowToDelete = tableStatisticCellByMovieId.closest('.movieContent');
-
-		const url = `/api/Movie/DeleteMovie?movieId=${movieId}`;
-		$.get(url)
-			.done(function (isDeleted) {
-				if (isDeleted) {
-					movieToDelete.remove();
-					rowToDelete.remove();
-				}
-			});
-	}
+        const url = `/api/Movie/DeleteMovie?movieId=${movieId}`;
+        $.get(url)
+            .done(function (isDeleted) {
+                if (isDeleted) {
+                    movieToDelete.remove();
+                    rowToDelete.remove();
+                } else {
+                    alert("Не удалось удалить фильм");
+                }
+            });
+    });
 });
