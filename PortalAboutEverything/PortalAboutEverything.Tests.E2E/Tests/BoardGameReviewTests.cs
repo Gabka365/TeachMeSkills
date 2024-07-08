86279933340f40d832b04bd74668ad79da87d044
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-07-08 03:45:22 +0300

    Added e2e for board games

--- PortalAboutEverything/PortalAboutEverything.Tests.E2E/Tests/BoardGameReviewTests.cs
+++ PortalAboutEverything/PortalAboutEverything.Tests.E2E/Tests/BoardGameReviewTests.cs
@@ -3,19 +3,20 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using PortalAboutEverything.Tests.E2E.PagesSelectors;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace PortalAboutEverything.Tests.E2E.Tests
 {
     internal class BoardGameReviewTests
     {
         private IWebDriver _driver;
-        //private WebDriverWait _wait;
+        private WebDriverWait _wait;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
             _driver = new ChromeDriver();
-            //_wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
         }
 
         [Test]
@@ -26,36 +27,32 @@ namespace PortalAboutEverything.Tests.E2E.Tests
             _driver.FindElement(Layout.BoardGameLink).Click();
             _driver.FindElement(BoardGameIndexPage.FirstBoardGame).Click();
 
-            //_wait.Until(driver => _driver.FindElement(BoardGamePage.ReviewsContainer).FindElements(By.XPath("./*")).Count > 0);
-            Thread.Sleep(5000);
-
             var reviewsBeforeAddOneMore = _driver.FindElements(BoardGamePage.Review).Count();
 
-            _driver.FindElement(BoardGamePage.CreateReviewButton).Click();
+            var button = _wait.Until(ExpectedConditions.ElementToBeClickable(BoardGamePage.CreateReviewButton));
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", button);
 
             _driver.FindElement(ReviewPage.TextInput).SendKeys("Test review!");
             _driver.FindElement(ReviewPage.SendButton).Click();
 
-            Thread.Sleep(5000);
+            var reviewsAfterAddOneMore = _driver.FindElements(BoardGamePage.Review).Count;
 
-            var reviewsAfterAddOneMore = _driver.FindElements(BoardGamePage.Review).Count();
+            Assert.That(reviewsBeforeAddOneMore, Is.EqualTo(reviewsAfterAddOneMore - 1));
 
-            Assert.That(reviewsBeforeAddOneMore + 1, Is.EqualTo(reviewsAfterAddOneMore));
-
-            _driver.FindElement(BoardGamePage.LastUpdateButton).Click();
+            var button2 = _wait.Until(ExpectedConditions.ElementToBeClickable(BoardGamePage.LastUpdateButton));
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", button2);
 
             _driver.FindElement(ReviewPage.TextInput).SendKeys("Test review!");
             _driver.FindElement(ReviewPage.SendButton).Click();
-
-            Thread.Sleep(5000);
 
             var textOfLastReview = _driver.FindElement(BoardGamePage.LastReviewText).Text;
 
             Assert.That(textOfLastReview, Is.EqualTo("Test review!" + "Test review!"));
 
-            _driver.FindElement(BoardGamePage.LastDeleteButton).Click();
+            var button3 = _wait.Until(ExpectedConditions.ElementToBeClickable(BoardGamePage.LastDeleteButton));
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", button3);
             Thread.Sleep(1000);
-            var reviewsAfterRemoveOneMore = _driver.FindElements(BoardGamePage.Review).Count();
+            var reviewsAfterRemoveOneMore = _driver.FindElements(BoardGamePage.Review).Count;
 
             Assert.That(reviewsAfterRemoveOneMore, Is.EqualTo(reviewsBeforeAddOneMore));
         }
