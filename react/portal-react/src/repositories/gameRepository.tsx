e055a8acf742065a651d02919552cb94661825a3
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-07-10 19:12:03 +0300

    Add prettier config

--- react/portal-react/src/repositories/gameRepository.tsx
+++ react/portal-react/src/repositories/gameRepository.tsx
@@ -1,20 +1,20 @@
-import axios from "axios";
-import GameModel from "../models/GameModel";
+import axios from 'axios';
+import GameModel from '../models/GameModel';
 
 const BASE_API_URL = 'http://localhost:5213/';
 const GAME_API_URL = `${BASE_API_URL}api/games/`;
 
 function getAll() {
-	return axios.get<GameModel[]>(`${GAME_API_URL}GetAll`)
+    return axios.get<GameModel[]>(`${GAME_API_URL}GetAll`);
 }
 
 function get(id: number) {
-	return axios.get(`${GAME_API_URL}get/${id}`)
+    return axios.get(`${GAME_API_URL}get/${id}`);
 }
 
 const gameRepository = {
-	getAll,
-	get
-}
+    getAll,
+    get,
+};
 
-export default gameRepository
+export default gameRepository;
