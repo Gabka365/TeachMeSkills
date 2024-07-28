39d03538e0692656a79dbb62ae7a9a69b38fdfa3
TatianaNai <tatyana_tat333@mail.ru>
2024-07-28 12:27:26 +0300

    Fix tabs

--- PortalAboutEverything/PortalAboutEverything/Models/Movie/MoviesFanViewModel.cs
+++ PortalAboutEverything/PortalAboutEverything/Models/Movie/MoviesFanViewModel.cs
@@ -1,12 +1,12 @@
 ﻿namespace PortalAboutEverything.Models.Movie
 {
-	public class MoviesFanViewModel
-	{
-		public string Name { get; set; }
+    public class MoviesFanViewModel
+    {
+        public string Name { get; set; }
 
-		public List<MovieIndexViewModel> Movies { get; set; }
+        public List<MovieIndexViewModel> Movies { get; set; }
 
-		public string Spell { get; set; }
+        public string Spell { get; set; }
 
         public string UsageOfSpell { get; set; }
     }
